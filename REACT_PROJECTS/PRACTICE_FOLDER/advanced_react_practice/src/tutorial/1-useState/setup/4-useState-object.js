8935e82c4d403b12dd1ab4f7e26a1dc5b4d101bb
Shrenik <61610570+Shrenik811rp@users.noreply.github.com>
2021-07-28 22:03:12 +0530

    completed js till useState next useEffect

--- REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/1-useState/setup/4-useState-object.js
+++ REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const [person, setPerson] = useState({
+    name: "Sam",
+    age: 23,
+    message: "random message"
+  })
+
+  const click_func = () => {
+    //change only the message on click
+
+    //use "spread operator" on the person object and access only the message attribute
+    setPerson({ ...person, name: "New message" })
+
+    /*If you do this:
+
+    setPerson({ message: "New message" })
+
+    It will replace the entire object with this, so it will replace it
+     */
+  }
+  return (
+    <React.Fragment>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.message}</h2>
+
+      <button className="btn" onClick={click_func}>Change message</button>
+    </React.Fragment>
+  )
+
 };
 
 export default UseStateObject;
--- REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/1-useState/setup/5-useState-counter.js
+++ REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,50 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+
+  //useState will hold an initial value of 0 for counter example
+  const [val, setVal] = useState(0)
+
+  const reset_func = () => {
+    setVal(0)
+  }
+  const incr_func = () => {
+    setVal(val + 1)
+  }
+  const decr_func = () => {
+    setVal(val - 1)
+  }
+
+  const complx_func = () => {
+    setTimeout(() => {
+
+      /*This line of code will also increase the counter value but the rest of the clicks will not increase its value */
+      //setVal(val + 1)
+
+      /*THis will increase the value how many ever times we want to increase a value with a delay */
+      setVal((prev_val) => {
+        return prev_val + 1
+      })
+    }, 1000)
+  }
+  return (
+    <React.Fragment>
+      <section>
+        <h2>Counter</h2>
+        <h2>{val}</h2>
+        <button className="btn" onClick={decr_func}>Decrease</button>
+        <button className="btn" onClick={reset_func}>Reset</button>
+        <button className="btn" onClick={incr_func}>Increase</button>
+      </section>
+
+      <section>
+        <h2>Complex Counter</h2>
+        <h2>{val}</h2>
+        <button className="btn" onClick={complx_func}>Increase</button>
+      </section>
+
+    </React.Fragment>
+  )
 };
 
 export default UseStateCounter;
