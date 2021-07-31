800df2903fa301c9ec2f37f68df8eb6ac599e6bd
Shrenik <61610570+Shrenik811rp@users.noreply.github.com>
2021-07-31 17:50:59 +0530

    finished till useEffect and multiple returns

--- REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,93 @@
+
+
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+
+  // - set isLoading boolean variable to true
+  const [isLoading, setLoading] = useState(true)
+
+  // - set isError boolean variable to false
+  const [isError, setError] = useState(false)
+
+  const [username, setUser] = useState("New User")
+
+
+
+  useEffect(() => {
+
+    /*Here we want to fetch the user using the url
+    
+    - We are not using async await for fetch in this case
+
+    - We convert the response we get into json format
+
+    - Since fetch returns a Promise we use "then" and print out our user
+
+    - If we get an error we catch it 
+    */
+    fetch(url)
+
+      /*What if the link to user is wrong then to display error status in response we modify response*/
+      .then((response) => {
+
+        //if url for user is correct and status of response within range
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json()
+        }
+
+        //Since we didnt get the right data and got an error
+
+        // we setLoading function to false
+        else {
+          setLoading(false)
+          setError(true)
+          throw Error(response.statusText)
+        }
+
+      })
+      .then((username) => {
+        console.log(username)
+
+        //get all the attributes from user object
+        const { login } = username
+
+        //we display the user
+        setUser(login)
+
+        //if setLoading is false then we display the name of user and not "New User"
+
+        setLoading(false)
+
+      })
+      .catch((error) => console.log(`You got error`))
+
+    /*We are using the empty dependency array so that UseEffect runs only once 
+    
+    - And not each time useEffect is called
+    */
+  }, [])
+
+
+  // ifLoading -> is true
+  if (isLoading) {
+    return (
+      <h2>Loading user...</h2>
+    )
+  }
+  // ifError -> is true
+  if (isError) {
+    return (
+      <h2>Error user...</h2>
+    )
+  }
+  return (
+    <React.Fragment>
+      <h2>{username}</h2>
+    </React.Fragment>
+  )
 };
 
 export default MultipleReturns;
