aa0777490b670f3edcb9825b51795415a1579f42
Shrenik <61610570+Shrenik811rp@users.noreply.github.com>
2021-08-04 17:01:30 +0530

    Finished till useRef

--- REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ REACT_PROJECTS/PRACTICE_FOLDER/advanced_react_practice/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,71 @@
+
+
+
 import React, { useEffect, useRef } from 'react';
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
+
+
+
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+
+  const ip_ref_example = useRef(null)
+
+  const div_ref_example = useRef(null)
+
+  const submit_func = (event) => {
+    event.preventDefault()
+    /*We print out the current value entered in the input space 
+    
+    - So when we hit submit the current inputted value will be printed out
+    */
+    console.log(`Message typed in input: ${ip_ref_example.current.value}`)
+
+    console.log(`type div: ${div_ref_example.current}`)
+
+  }
+
+
+  useEffect(() => {
+
+    //prints out whatever we type in input
+    console.log(`Type  : ${ip_ref_example.current}`)
+
+    //here we add a highlight to the border of the input space
+    ip_ref_example.current.focus()
+
+    /*In the case of useRef ->
+
+    We dont need to create an empty dependency array '[]' like for useState
+
+    useEffect(()=>{
+
+    },[])
+
+
+    - Because UseRef doesnt trigger re-render unlike useState
+    */
+  })
+
+
+  console.log(`ref example: ${ip_ref_example}`)
+  return (
+    <React.Fragment>
+      <h2>UseRef Demo example</h2>
+      <form className='form' onSubmit={submit_func}>
+        <div>
+          <input type="text" ref={ip_ref_example} />
+          <button type='submit'>
+            Submit
+          </button>
+        </div>
+        <div ref={div_ref_example}>demo of UseRef</div>
+      </form>
+    </React.Fragment>
+  )
 };
 
 export default UseRefBasics;
