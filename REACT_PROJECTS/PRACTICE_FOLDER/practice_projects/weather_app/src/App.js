390bad35c7b203a0f6ba82df66564d35df5bd0fd
Shrenik <61610570+Shrenik811rp@users.noreply.github.com>
2021-10-11 21:56:21 +0530

    weather app not complete

---
+++ REACT_PROJECTS/PRACTICE_FOLDER/practice_projects/weather_app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from "react"
+
+const api ={
+  key:"d34732fa515adb032e25f5b441af6cd0",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
